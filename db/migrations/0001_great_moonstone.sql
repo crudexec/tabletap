9595f715edaa28f61be5c5e7f66010c5b39e91df
Lumi <cerebrum-mobi@Lumis-MacBook-Pro-2.local>
2026-01-22 00:06:58 -0800

    fix mgiration

--- db/migrations/0001_great_moonstone.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "settings" ADD COLUMN "table_seats" jsonb DEFAULT '{}'::jsonb NOT NULL;--> statement-breakpoint
-ALTER TABLE "settings" ADD COLUMN "warning_threshold" integer DEFAULT 2 NOT NULL;--> statement-breakpoint
-ALTER TABLE "settings" ADD COLUMN "critical_threshold" integer DEFAULT 5 NOT NULL;
